c7ff7fcc61497e42c846f13b57f46dffba378882
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2018-08-10 21:37:48 +0600

    Create push handler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -8,8 +9,13 @@ func ping(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("pong"))
 }
 
+func push(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Someone just pushed to repo!")
+}
+
 func main() {
 	http.HandleFunc("/ping", ping)
+	http.HandleFunc("/push", push)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
