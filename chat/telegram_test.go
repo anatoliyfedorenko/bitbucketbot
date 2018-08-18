098ccd7e1d215fdbda51f2d9dfdee3b0eb99ef60
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2018-08-18 12:45:39 +0600

    Add more tests, remove repeated code

--- chat/telegram_test.go
+++ chat/telegram_test.go
@@ -35,54 +35,12 @@ func TestNewBotReturnsError(t *testing.T) {
 }
 
 func TestSendUpdate(t *testing.T) {
-	os.Setenv("BOT_TELEGRAM_TOKEN", "testToken")
-	os.Setenv("BOT_CHAT", "-12345")
-	conf, err := config.GetConfig()
-	assert.NoError(t, err)
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
-
-	r := httpmock.NewStringResponder(200, `{
-		"ok": true,
-		"result": {
-		  "id": 1000,
-		  "is_bot": true,
-		  "first_name": "testBot",
-		  "username": "testbot_bot"
-		}
-	  }`)
-
-	url := fmt.Sprintf("https://api.telegram.org/bot%v/getMe", conf.TelegramToken)
-	httpmock.RegisterResponder("POST", url, r)
-
-	bot, err := NewBot(conf)
-	assert.NoError(t, err)
+	bot := setupTestBot(t)
 	bot.SendUpdate("test")
 }
 
 func TestPullRequestCreated(t *testing.T) {
-	os.Setenv("BOT_TELEGRAM_TOKEN", "testToken")
-	os.Setenv("BOT_CHAT", "-12345")
-	conf, err := config.GetConfig()
-	assert.NoError(t, err)
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
-
-	r := httpmock.NewStringResponder(200, `{
-		"ok": true,
-		"result": {
-		  "id": 1000,
-		  "is_bot": true,
-		  "first_name": "testBot",
-		  "username": "testbot_bot"
-		}
-	  }`)
-
-	url := fmt.Sprintf("https://api.telegram.org/bot%v/getMe", conf.TelegramToken)
-	httpmock.RegisterResponder("POST", url, r)
-
-	bot, err := NewBot(conf)
-	assert.NoError(t, err)
+	bot := setupTestBot(t)
 
 	reader, err := os.Open("prcreated.json")
 	assert.NoError(t, err)
@@ -96,14 +54,59 @@ func TestPullRequestCreated(t *testing.T) {
 	rr := httptest.NewRecorder()
 	bot.PullRequestCreated(rr, req)
 
-	// Check the status code is what we expect.
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
+}
+
+func TestPullRequestMerged(t *testing.T) {
+	bot := setupTestBot(t)
+
+	reader, err := os.Open("prmerged.json")
+	assert.NoError(t, err)
+
+	req, err := http.NewRequest("POST", "/pull_request_merged", reader)
+	if err != nil {
+		t.Fatal(err)
 	}
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	bot.PullRequestMerged(rr, req)
+
+}
+
+func TestPullRequestCommented(t *testing.T) {
+	bot := setupTestBot(t)
+
+	reader, err := os.Open("prcommented.json")
+	assert.NoError(t, err)
+
+	req, err := http.NewRequest("POST", "/pull_request_commented", reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	bot.PullRequestCommented(rr, req)
+
 }
 
 func TestPullRequestApproved(t *testing.T) {
+	bot := setupTestBot(t)
+
+	reader, err := os.Open("prapproved.json")
+	assert.NoError(t, err)
+
+	req, err := http.NewRequest("POST", "/pull_request_approved", reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+	bot.PullRequestApproved(rr, req)
+
+}
+
+func setupTestBot(t *testing.T) *Bot {
 	os.Setenv("BOT_TELEGRAM_TOKEN", "testToken")
 	os.Setenv("BOT_CHAT", "-12345")
 	conf, err := config.GetConfig()
@@ -126,22 +129,5 @@ func TestPullRequestApproved(t *testing.T) {
 
 	bot, err := NewBot(conf)
 	assert.NoError(t, err)
-
-	reader, err := os.Open("prmerged.json")
-	assert.NoError(t, err)
-
-	req, err := http.NewRequest("POST", "/pull_request_merged", reader)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-	rr := httptest.NewRecorder()
-	bot.PullRequestMerged(rr, req)
-
-	// Check the status code is what we expect.
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
+	return bot
 }
