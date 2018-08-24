d077b4eb7d138ddd6efaf8b837923c2524a0a692
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2018-08-24 11:24:41 +0600

    [#2] Modify message for pr commented handler

--- chat/telegram.go
+++ chat/telegram.go
@@ -66,7 +66,7 @@ func (bot *Bot) PullRequestCommented(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		logrus.Errorf("Decode failed: %v", err)
 	}
-	text := fmt.Sprintf("%s написал комментарий к пул реквесту [#%v](%v): %v.", pr.Actor.DisplayName, pr.PullRequest.ID, pr.PullRequest.Links.HTML.Href, pr.Comment.Content.Raw)
+	text := fmt.Sprintf("%s написал [комментарий](%v) к пул реквесту [#%v](%v): %v.", pr.Actor.DisplayName, pr.Comment.Links.HTML.Href, pr.PullRequest.ID, pr.PullRequest.Links.HTML.Href, pr.Comment.Content.Raw)
 	logrus.Println(text)
 	if pr.Actor.DisplayName != "" && pr.PullRequest.ID != 0 && pr.PullRequest.Links.HTML.Href != "" && pr.Comment.Content.HTML != "" {
 		bot.SendUpdate(text)
